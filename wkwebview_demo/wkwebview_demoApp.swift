80af7841e38c0b481f0080354d881081b602a646
Edward Yee <yeeedward@hotmail.com>
2023-04-29 23:16:07 +0800

    Initial Commit

---
+++ wkwebview_demo/wkwebview_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  wkwebview_demoApp.swift
+//  wkwebview_demo
+//
+//  Created by Edward Yee on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct wkwebview_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
